0e67e1d445ff3973f79e1bfb3b1ec5796462088d
Stefan Hasenoot <stefanhasenoot@hotmail.com>
2024-12-19 21:16:18 +0100

    bich

---
+++ intent.py
@@ -0,0 +1,4 @@
+# https://snips-nlu.readthedocs.io/en/latest/installation.html
+
+def getIntent():
+    print("Intent: WAHOO")
---
+++ wakeword.py
@@ -0,0 +1,45 @@
+# .\porcupine_env\Scripts\activate
+# Console for API key and model: https://console.picovoice.ai
+
+import pvporcupine
+import pyaudio
+import struct
+import intent
+import stt
+
+# Replace with your access key
+access_key = r"GJtuxYjiBSMWPSVqtNKwnozW9dWkkps6VTmgROSlRbdljHsMbwlD/w=="
+
+# Path to the wake word .ppn file
+keyword_path = r"./Wake word detection\Hey-Sprout_en_windows_v3_0_0.ppn"
+
+# Initialize Porcupine
+porcupine = pvporcupine.create(access_key=access_key, keyword_paths=[keyword_path]) # [keyword_path]
+
+# Audio stream setup
+pa = pyaudio.PyAudio()
+audio_stream = pa.open(
+    rate=porcupine.sample_rate,
+    channels=1,
+    format=pyaudio.paInt16,
+    input=True,
+    frames_per_buffer=porcupine.frame_length
+)
+
+print("Listening for wake word...")
+
+try:
+    while True:
+        pcm = audio_stream.read(porcupine.frame_length)
+        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+        result = porcupine.process(pcm)
+        if result >= 0:
+            print("Wake word detected!")
+            intent.getIntent()
+except KeyboardInterrupt:
+    print("Stopping...")
+finally:
+    audio_stream.close()
+    porcupine.delete()
+    pa.terminate()
