3738036b5263b639a29e73f250acf7117378f1fc
Stefan Hasenoot <stefanhasenoot@hotmail.com>
2024-12-21 19:44:05 +0100

    Update wakeword.py

--- wakeword.py
+++ wakeword.py
@@ -8,7 +8,7 @@ import intent
 import stt
 
 # Replace with your access key
-access_key = r"GJtuxYjiBSMWPSVqtNKwnozW9dWkkps6VTmgROSlRbdljHsMbwlD/w=="
+access_key = r""
 
 # Path to the wake word .ppn file
 keyword_path = r"./Wake word detection/Hey-Sprout_en_raspberry-pi_v3_0_0.ppn"
@@ -16,18 +16,28 @@ keyword_path = r"./Wake word detection/Hey-Sprout_en_raspberry-pi_v3_0_0.ppn"
 # Initialize Porcupine
 porcupine = pvporcupine.create(access_key=access_key, keyword_paths=[keyword_path]) # [keyword_path]
 
-# Audio stream setup
+# Audio stream setup with automatic resampling
 pa = pyaudio.PyAudio()
 print("Sampling rate:",porcupine.sample_rate)
 audio_stream = pa.open(
-    rate=porcupine.sample_rate,
+    rate=41000,  # Set the actual sampling rate of the microphone
     channels=1,
     format=pyaudio.paInt16,
     input=True,
-    frames_per_buffer=porcupine.frame_length,
+    frames_per_buffer=int(41000 * (1/16000)),  # Calculate frames per buffer based on the desired sampling rate
     input_device_index=1,
     input_host_api_specific_stream_info={
-        'input_device': 'plughw:2,0'  # Use pluggable hardware device for automatic resampling
+        pyaudio.paPlugIn,
+        pyaudio.paHostApiTypeId,
+        'plughw:2,0',  # Use pluggable hardware device for automatic resampling
+        audio_stream.get_device_info_by_index(1).maxInputChannels,
+        audio_stream.get_device_info_by_index(1).maxOutputChannels,
+        41000,  # Actual sampling rate of the microphone
+        None,
+        0,
+        pyaudio.paStream,
+        True,
+        None
     }
 )
 
