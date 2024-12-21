b277e5ac3059f53091063a143ef35604c57f1b48
Stefan Hasenoot <stefanhasenoot@hotmail.com>
2024-12-21 22:18:37 +0100

    Update intent.py

--- intent.py
+++ intent.py
@@ -1,12 +1,17 @@
 # https://snips-nlu.readthedocs.io/en/latest/installation.html
 
+from rasa.nlu.model import Interpreter
+
+interpreter = Interpreter.load("models/nlu")
+
 def getIntent():
-    print(listenForCommand())
-    print("Intent: WAHOO")
+    command = listenForCommand()
+    result = interpreter.parse(command)
+    print(result)
 
 
 def listenForCommand():
     # Record audio while user is talking (test: 10 seconds)
-    # Speech to text
+    # Speech to text (vosk)
     # Return speech
     return input("User: ") # Test output using input string
