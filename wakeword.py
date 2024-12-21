28c7efeaee7a76bfaf56b476180214d03e019fa0
Stefan Hasenoot <stefanhasenoot@hotmail.com>
2024-12-21 19:56:00 +0100

    Update wakeword.py

--- wakeword.py
+++ wakeword.py
@@ -20,25 +20,12 @@ porcupine = pvporcupine.create(access_key=access_key, keyword_paths=[keyword_pat
 pa = pyaudio.PyAudio()
 print("Sampling rate:",porcupine.sample_rate)
 audio_stream = pa.open(
-    rate=41000,  # Set the actual sampling rate of the microphone
+    rate=porcupine.sample_rate,  # Set the actual sampling rate of the microphone
     channels=1,
     format=pyaudio.paInt16,
     input=True,
     frames_per_buffer=int(41000 * (1/porcupine.sample_rate)),  # Calculate frames per buffer based on the desired sampling rate
     input_device_index=1,
-    input_host_api_specific_stream_info={
-        pyaudio.paPlugIn,
-        pyaudio.paHostApiTypeId,
-        'plughw:2,0',  # Use pluggable hardware device for automatic resampling
-        audio_stream.get_device_info_by_index(1).maxInputChannels,
-        audio_stream.get_device_info_by_index(1).maxOutputChannels,
-        41000,  # Actual sampling rate of the microphone
-        None,
-        0,
-        pyaudio.paStream,
-        True,
-        None
-    }
 )
 
 print("Listening for wake word...")
